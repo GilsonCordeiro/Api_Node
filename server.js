e64242cd4ef7044497e802649dadeaddb7747428
GilsonCordeiro <gil.silva.cord2021@gmail.com>
2025-08-20 14:58:47 -0300

    Install and configure Mongo db database

--- server.js
+++ server.js
@@ -1,18 +1,27 @@
 import express from 'express'; 
+import { PrismaClient } from './generated/prisma'
+
+const prisma = new PrismaClient()
 
 const app = express();
 app.use(express.json())
 
 const users = []
 
-app.post('/users',(req, res) => {
+app.post('/users', async (req, res) => {
 
-    users.push(req.body)
-    res.send('Ok post')
+    await prisma.user.create({
+        data: {
+            email: req.body.email,
+            name: req.body.name,
+            age: req.body.age
+        }
+    })
+    res.status(201).json(req.body)
 })
 
 app.get('/users', (req, res) => {
-    res.json(users )
+    res.status(200).json(users )
 })
 
 
@@ -21,3 +30,8 @@ app.get('/users', (req, res) => {
 
 
 app.listen(3000)
+
+//Criação do banco de dados Mongo DB
+//site: https://cloud.mongodb.com/v2/68a5d745bc7d6844d9ad98c5#/overview?automateSecurity=true
+//Usuário db: gilsilvacord2021
+//Senha: nDiPPhrCwZ6LS8r0
